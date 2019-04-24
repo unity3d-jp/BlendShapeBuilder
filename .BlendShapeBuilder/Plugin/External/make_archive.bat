b7f0bc4f7bbd12c308c770338ca8f499d0e8f156
i-saint <saint.skr@gmail.com>
2019-04-25 04:19:46 +0900

    update build script

---
+++ .BlendShapeBuilder/Plugin/External/make_archive.bat
@@ -0,0 +1,3 @@
+7z\7za.exe a -mx=9 External.7z^
+ libs^
+ ispc.exe
--- .BlendShapeBuilder/Plugin/build.bat
+++ .BlendShapeBuilder/Plugin/build.bat
@@ -1,3 +1,3 @@
-call buildtools.bat
+call toolchain.bat
 
-msbuild VertexTweakerCore.vcxproj /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
+msbuild VertexTweakerCore.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
